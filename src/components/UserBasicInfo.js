6bb6e9aac038d2dde8048cdae685e7f0acd0cbb8
Janani Rajendran <ra.janani@yahoo.co.in>
2023-11-07 22:37:01 +0000

    password and retype password donot match

--- src/components/UserBasicInfo.js
+++ src/components/UserBasicInfo.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import {useState} from 'react';
 import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 
-const UserBasicInfo = (props) => {
+const UserBasicInfo = props => {
+  const [pass, setPass] = useState('');
+  const [vpass, setVpass] = useState('');
 
-const {inputStyle,bgColor,colorChangeCB}=props;
+  const {inputStyle, bgColor, colorChangeCB} = props;
 
   return (
-    <View style={{backgroundColor:bgColor}}>
+    <View style={{backgroundColor: bgColor}}>
       <Text>UserBasicInfo</Text>
-      <TextInput value={props.firstName} onChangeText={()=>{}} placeholder='First Name' style={inputStyle}/>
-      <TextInput value={props.lastName} onChangeText={()=>{}} placeholder='Last Name'style={inputStyle}/>
-      <TouchableOpacity onPress={()=>{
-        colorChangeCB('blue');
-      }}><Text style={{fontSize:20}}>User Info Button</Text></TouchableOpacity>
-    
+      <TextInput
+        value={props.firstName}
+        onChangeText={() => {}}
+        placeholder="First Name"
+        style={inputStyle}
+      />
+      <TextInput
+        value={props.lastName}
+        onChangeText={() => {}}
+        placeholder="Last Name"
+        style={inputStyle}
+      />
+      <TextInput
+        value={pass}
+        onChangeText={changedText => {
+          setPass(changedText);
+        }}
+        placeholder="Password"
+        style={inputStyle}
+        secureTextEntry></TextInput>
+      {pass != vpass && <Text>Password doesnot match</Text>}
+
+      <TextInput
+        value={vpass}
+        onChangeText={changedText => {
+          setVpass(changedText);
+        }}
+        placeholder="Retype Password"
+        style={inputStyle}
+        secureTextEntry></TextInput>
+      <TouchableOpacity
+        onPress={() => {
+          colorChangeCB('blue');
+        }}>
+        <Text style={{fontSize: 20}}>User Info Button</Text>
+      </TouchableOpacity>
     </View>
   );
 };
