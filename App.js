0abd020fe516ee87cae6ba0de916887ebd1c8e0e
Janani Rajendran <ra.janani@yahoo.co.in>
2023-11-08 11:10:47 +0000

    Flex-direction code for row, column, reverse row and reverse column

--- App.js
+++ App.js
@@ -9,7 +9,11 @@ import React, {useState} from 'react';
 
 import {Text, View, ScrollView} from 'react-native';
 
-import {MyTestComponent,MyTestFuncComponent,UserProfile} from './src/components';
+import {
+  MyTestComponent,
+  MyTestFuncComponent,
+  UserProfile,
+} from './src/components';
 function App() {
   const userObject = {
     firstName: 'Janani',
@@ -22,22 +26,29 @@ function App() {
     postalCode: 'SE137SL',
   };
 
-  const inputStyle={
-    height:40,
-    backgroundColor:'pink',
-    margin:10,
-    padding:5
+  const inputStyle = {
+    height: 40,
+    backgroundColor: 'pink',
+    margin: 10,
+    padding: 5,
   };
 
   return (
-    <View style={{flex: 1, margin: 40}}>
-      <ScrollView>
-        <MyTestComponent/>
-        <MyTestFuncComponent/>
-        <UserProfile userData={userObject} inputStyle={inputStyle}/>
-      </ScrollView>
+    <View style={{flex: 1, flexDirection:'row'}}>
+      <View style={{flex: 1, backgroundColor: 'red'}}>
+        <View style={{flex: 1, backgroundColor: 'pink', margin: 5}}></View>
+        <View style={{flex: 2, backgroundColor: 'yellow', margin: 5}}></View>
+        <View style={{flex: 3, backgroundColor: 'green', margin: 5}}></View>
+      </View>
+
+      <View style={{flex: 1, backgroundColor: 'red', flexDirection: 'row'}}>
+        <View style={{flex: 1, backgroundColor: 'pink', margin: 5}}></View>
+        <View style={{flex: 2, backgroundColor: 'yellow', margin: 5}}></View>
+        <View style={{flex: 3, backgroundColor: 'green', margin: 5}}></View>
+      </View>
+
     </View>
   );
-}
+} 
 
 export default App;
