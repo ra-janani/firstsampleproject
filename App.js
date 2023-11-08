a0dcf915d36c525624b00f86e1de35e016c3241a
Janani Rajendran <ra.janani@yahoo.co.in>
2023-11-08 16:46:16 +0000

    Navigation logics-added inline component creation in the <stack.screen>

--- App.js
+++ App.js
@@ -6,90 +6,72 @@
  */
 
 import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Text, View, FlatList, Image, SafeAreaView} from 'react-native';
 
-import {
-  Text,
-  View,
-  Button,
-  ScrollView,
-  TouchableOpacity,
-  FlatList,
-  Image,
-  SafeAreaView,
-} from 'react-native';
+const Stack = createNativeStackNavigator();
 
-import {
-  MyTestComponent,
-  MyTestFuncComponent,
-  UserProfile,
-} from './src/components';
-function App() {
-  const userObject = {
-    firstName: 'Janani',
-    lastName: 'Rajendran',
-    phone: '07919849860',
-    email: 'ra.janani@yahoo.co.in',
-    city: 'London',
-    country: 'England',
-    street: 'Thurston Road',
-    postalCode: 'SE137SL',
-  };
-
-  const inputStyle = {
-    height: 40,
-    backgroundColor: 'pink',
-    margin: 10,
-    padding: 5,
-  };
-
+const DashBoardScreen = () => {
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
       }}>
-      <SafeAreaView>
-      
-        <FlatList
-          data={[
-            {
-              title: 'London',
-            },
-            {
-              title: 'Cardiff',
-            },
-            {
-              title: 'Delhi',
-            },
-            {
-              title: 'Chennai',
-            },
-          ]}
-          renderItem={({item}) => {
-            return (
-              <View
-                style={{
-                  height: 80,
-                  backgroundColor: 'yellow',
-                  marginVertical: 5,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  flexDirection: 'row',
-                }}>
-                <Image
-                  style={{width: 40, height: 40}}
-                  source={{
-                    uri: 'https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340',
-                  }}
-                />
-                <View style={{flex: 1, alignItems: 'center'}}>
-                  <Text>{item.title}</Text>
-                </View>
+      <FlatList
+        data={[
+          {
+            title: 'London',
+          },
+          {
+            title: 'Cardiff',
+          },
+          {
+            title: 'Delhi',
+          },
+          {
+            title: 'Chennai',
+          },
+        ]}
+        renderItem={({item}) => {
+          return (
+            <View
+              style={{
+                height: 80,
+                backgroundColor: 'yellow',
+                marginVertical: 5,
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'row',
+              }}>
+              <Image
+                style={{width: 40, height: 40}}
+                source={{
+                  uri: 'https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340',
+                }}
+              />
+              <View style={{flex: 1, alignItems: 'center'}}>
+                <Text>{item.title}</Text>
               </View>
-            );
-          }}
-        />
-      </SafeAreaView>
-    </View>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        
+        <Stack.Screen name="Settings" component={()=>{
+          return(<View><Text>This is settings screen</Text></View>);
+        }} />
+        <Stack.Screen name="DashBoard" component={DashBoardScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
