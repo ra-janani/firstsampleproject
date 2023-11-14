c7eedb0fa1a5daeaf39f1d8569a21ff581ed214a
Janani Rajendran <ra.janani@yahoo.co.in>
2023-11-14 00:49:49 +0000

    converted parent index.js class component to functional component

--- src/containers/TestClassComp/index.js
+++ src/containers/TestClassComp/index.js
@@ -1,45 +1,77 @@
-import React from 'react';
+import {React,useState} from 'react';
 import {SafeAreaView, TextInput, View} from 'react-native';
 import LevelOne from './LevelOne';
 
-class TestClassComp extends React.Component {
-  constructor(props) {
-    super(props);
+// class TestClassComp extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      inputText: '',
-      inputText2:''
-    };
-  }
+//     this.state = {
+//       inputText: '',
+//       inputText2:''
+//     };
+//   }
 
-  componentWillUnmount(){
-    console.log('class component got unmounted');
-  }
-  render() {
-    const {inputText,inputText2}=this.state;
-   console.log('testclass component rendered');
-    return (
-      <SafeAreaView style={{flex: 1}}>
-        <TextInput
-          style={{backgroundColor: 'pink', height: 40, margin: 10, padding: 5}}
-          value={this.state.inputText}
-          placeholder="Type anything"
-          onChangeText={changedText => {
-            this.setState({inputText: changedText});
-          }}
-        />
-        <TextInput
-          style={{backgroundColor: 'pink', height: 40, margin: 10, padding: 5}}
-          value={this.state.inputText2}
-          placeholder="Type anything"
-          onChangeText={changedText => {
-            this.setState({inputText2: changedText});
-          }}
-        />
-        <LevelOne inputText={inputText} inputText2={inputText2}></LevelOne>
-        <LevelOne></LevelOne>
-      </SafeAreaView>
-    );
-  }
-}
+//   componentWillUnmount(){
+//     console.log('class component got unmounted');
+//   }
+//   render() {
+//     const {inputText,inputText2}=this.state;
+//    console.log('testclass component rendered');
+//     return (
+//       <SafeAreaView style={{flex: 1}}>
+//         <TextInput
+//           style={{backgroundColor: 'pink', height: 40, margin: 10, padding: 5}}
+//           value={this.state.inputText}
+//           placeholder="Type anything"
+//           onChangeText={changedText => {
+//             this.setState({inputText: changedText});
+//           }}
+//         />
+//         <TextInput
+//           style={{backgroundColor: 'pink', height: 40, margin: 10, padding: 5}}
+//           value={this.state.inputText2}
+//           placeholder="Type anything"
+//           onChangeText={changedText => {
+//             this.setState({inputText2: changedText});
+//           }}
+//         />
+//         <LevelOne inputText={inputText} inputText2={inputText2}></LevelOne>
+//         <LevelOne></LevelOne>
+//       </SafeAreaView>
+//     );
+//   }
+// }
+// export default TestClassComp;
+
+const TestClassComp = (props) => {
+
+    const[inputText,setInputText]=useState('');
+    const[inputText2,setInputText2]=useState('');
+
+  console.log('testclass component rendered');
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <TextInput
+        style={{backgroundColor: 'pink', height: 40, margin: 10, padding: 5}}
+        value={inputText}
+        placeholder="Type anything"
+        onChangeText={changedText => {
+            setInputText(changedText);
+        }}
+      />
+      <TextInput
+        style={{backgroundColor: 'pink', height: 40, margin: 10, padding: 5}}
+        value={inputText2}
+        placeholder="Type anything"
+        onChangeText={changedText => {
+            setInputText2(changedText);
+        }}
+      />
+      <LevelOne inputText={inputText} inputText2={inputText2}></LevelOne>
+      <LevelOne></LevelOne>
+    </SafeAreaView>
+  );
+};
+
 export default TestClassComp;
