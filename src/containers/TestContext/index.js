735b726a3996a20532c090a8144ae08bb0598590
Janani Rajendran <ra.janani@yahoo.co.in>
2023-11-15 16:32:28 +0000

    contextApi implementation

--- src/containers/TestContext/index.js
+++ src/containers/TestContext/index.js
@@ -1,52 +1,56 @@
-import {useState} from 'react';
+import {useState, createContext,useContext} from 'react';
 import {View, Text, TextInput} from 'react-native';
+const userContext = createContext();
 
 const TestContext = () => {
   const [user, setUser] = useState('Jessie Hall');
   return (
-    <View style={{backgroundColor: 'red', margin: 5}}>
-      <Text>Component 1 {user}</Text>
-      <TextInput
-        style={{
-          backgroundColor: 'lightblue',
-          margin: 5,
-          borderColor: 'black',
-          borderWidth: 2,
-        }}
-        value={user}
-        onChangeText={changedText => {
-          setUser(changedText);
-        }}
-      />
-      <TestContext2 user={user} />
-    </View>
+    <userContext.Provider value={user}>
+      <View style={{backgroundColor: 'red', margin: 5}}>
+        <Text>Component 1 {user}</Text>
+        <TextInput
+          style={{
+            backgroundColor: 'lightblue',
+            margin: 5,
+            borderColor: 'black',
+            borderWidth: 2,
+          }}
+          value={user}
+          onChangeText={changedText => {
+            setUser(changedText);
+          }}
+        />
+        <TestContext2 />
+      </View>
+    </userContext.Provider>
   );
 };
-const TestContext2 = ({user}) => {
+const TestContext2 = () => {
   return (
     <View style={{backgroundColor: 'green', margin: 5}}>
       <Text>Component 2</Text>
-      <TestContext3 user={user} />
+      <TestContext3 />
     </View>
   );
 };
-const TestContext3 = ({user}) => {
+const TestContext3 = () => {
   return (
     <View style={{backgroundColor: 'blue', margin: 5}}>
       <Text>Component 3</Text>
-      <TestContext4 user={user} />
+      <TestContext4 />
     </View>
   );
 };
-const TestContext4 = ({user}) => {
+const TestContext4 = () => {
   return (
     <View style={{backgroundColor: 'yellow', margin: 5}}>
       <Text>Component 4</Text>
-      <TestContext5 user={user} />
+      <TestContext5 />
     </View>
   );
 };
-const TestContext5 = ({user}) => {
+const TestContext5 = () => {
+  const user= useContext(userContext);  
   return (
     <View style={{backgroundColor: 'pink', margin: 5}}>
       <Text>Component 5</Text>
@@ -56,3 +60,59 @@ const TestContext5 = ({user}) => {
 };
 
 export default TestContext;
+
+// const TestContext = () => {
+//   const [user, setUser] = useState('Jessie Hall');
+//   return (
+//     <View style={{backgroundColor: 'red', margin: 5}}>
+//       <Text>Component 1 {user}</Text>
+//       <TextInput
+//         style={{
+//           backgroundColor: 'lightblue',
+//           margin: 5,
+//           borderColor: 'black',
+//           borderWidth: 2,
+//         }}
+//         value={user}
+//         onChangeText={changedText => {
+//           setUser(changedText);
+//         }}
+//       />
+//       <TestContext2 user={user} />
+//     </View>
+//   );
+// };
+// const TestContext2 = ({user}) => {
+//   return (
+//     <View style={{backgroundColor: 'green', margin: 5}}>
+//       <Text>Component 2</Text>
+//       <TestContext3 user={user} />
+//     </View>
+//   );
+// };
+// const TestContext3 = ({user}) => {
+//   return (
+//     <View style={{backgroundColor: 'blue', margin: 5}}>
+//       <Text>Component 3</Text>
+//       <TestContext4 user={user} />
+//     </View>
+//   );
+// };
+// const TestContext4 = ({user}) => {
+//   return (
+//     <View style={{backgroundColor: 'yellow', margin: 5}}>
+//       <Text>Component 4</Text>
+//       <TestContext5 user={user} />
+//     </View>
+//   );
+// };
+// const TestContext5 = ({user}) => {
+//   return (
+//     <View style={{backgroundColor: 'pink', margin: 5}}>
+//       <Text>Component 5</Text>
+//       <Text>{user}</Text>
+//     </View>
+//   );
+// };
+
+// export default TestContext;
