156a5a913a3af5868a4debf43aa892dd2ce6eb92
ra-janani <87525936+ra-janani@users.noreply.github.com>
2023-12-12 21:13:51 +0000

    typescript basics

--- src/containers/TypeScriptScreen/index.tsx
+++ src/containers/TypeScriptScreen/index.tsx
@@ -17,29 +17,38 @@ export default function TypeScriptScreen() {
   arrayOfBoolean = [true, false, false];
 
   //let person: {name: string; age: number};
- // person = {name: 'janani', age: 33};
+  // person = {name: 'janani', age: 33};
 
+  type Person = {name: string; age: number; courses?: string[]};
+  type Person2 = {fullName: string; email: string; address: string};
 
- type Person={name:string,age:number,courses?:string[]};
- type Person2={fullName:string,email:string,address:string};
+  type CommonPerson = Person | Person2; // UNION
 
- type CommonPerson=Person|Person2;  // UNION
-
- let person:Person;
- person={name:'sharvesh',age:8,courses:['android','ios']};
+  let person: Person;
+  person = {name: 'sharvesh', age: 8, courses: ['android', 'ios']};
 
   let people: CommonPerson[];
-  people= [
-    {name: 'jan', age: 33,courses:['eng','tamil']},
+  people = [
+    {name: 'jan', age: 33, courses: ['eng', 'tamil']},
     {name: 'raj', age: 33},
     {name: 'reen', age: 3},
-    {fullName:'jan raj',email:'ra.janani',address:'street'}
+    {fullName: 'jan raj', email: 'ra.janani', address: 'street'},
   ];
+
+  const add = (x:number, y:number):number => {
+    return x + y; // return x+' '+y; will give error coz we are concatenating string in the return stmt but the fn shud return number
+  };
+
+  const concatenate = (x:any, y:any):string => {
+    return x + y;
+  };
 
   return (
     <View>
       <Text>index</Text>
       <Text>{myPhoneNumber}</Text>
+      <Text>{add(5, 3)}</Text>
+      <Text>{concatenate("Jan","raj")}</Text>
     </View>
   );
 }
